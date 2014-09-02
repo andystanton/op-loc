53ca6b60fbdaad81f13978abed6ec6c85fad3991
Andy Stanton <andystanton@gmail.com>
2014-09-02 09:05:11 +0100

    #62 start work on donut

--- src/main/resources/WEB-INF/js/optLocApp.js
+++ src/main/resources/WEB-INF/js/optLocApp.js
@@ -47,8 +47,29 @@ app.controller("mapController", function($scope, $http, optLocService) {
         control: {}
     };
 
+    var donut;
+
     $scope.$on('updateLocationEvent', function() {
         var rawLocation = optLocService.location;
+        var rawLatLong = rawLocation.latlong;
+
+        if (typeof donut !== 'undefined') {
+            donut.setMap(null);
+        }
+        donut = new google.maps.Polygon({
+            paths: [
+                drawCircle(new google.maps.LatLng(rawLatLong.latitude, rawLatLong.longitude), 100, 1),
+                drawCircle(new google.maps.LatLng(rawLatLong.latitude, rawLatLong.longitude), 50, -1)
+            ],
+            strokeColor: "#0000FF",
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: "#FF0000",
+            fillOpacity: 0.35
+        });
+        donut.setMap($scope.map.control.getGMap());
+
+
 
         var location = {
             id: rawLocation.id,
@@ -98,6 +119,37 @@ app.controller("mapController", function($scope, $http, optLocService) {
             visible: (typeof location.id !== 'undefined')
         };
     });
+
+    function drawCircle(point, radius, dir) {
+        var d2r = Math.PI / 180;    // degrees to radians
+        var r2d = 180 / Math.PI;    // radians to degrees
+        var earthsradius = 3963;    // 3963 is the radius of the earth in miles
+
+        var points = 32;
+
+        // find the radius in lat/lon
+        var rlat = (radius / earthsradius) * r2d;
+        var rlng = rlat / Math.cos(point.lat() * d2r);
+
+
+        var extp = new Array();
+        if (dir == 1) {
+            var start=0;
+            var end=points+1 // one extra here makes sure we connect the
+        } else {
+            var start=points + 1;
+            var end=0
+        }
+        for (var i=start; (dir==1 ? i < end : i > end); i=i+dir) {
+            var theta = Math.PI * (i / (points/2));
+            ey = point.lng() + (rlng * Math.cos(theta)); // center a + radius x * cos(theta)
+            ex = point.lat() + (rlat * Math.sin(theta)); // center b + radius y * sin(theta)
+            extp.push(new google.maps.LatLng(ex, ey));
+
+        }
+        return extp;
+    }
+
 
     var init = function () {
         var searchString = 'London';
