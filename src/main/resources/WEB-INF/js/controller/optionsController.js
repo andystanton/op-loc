216f500017e43a97bfc2e3482ccb43e12398ada8
Andy Stanton <andystanton@gmail.com>
2014-09-04 20:15:41 +0100

    #59 improve style. improve slider style. #66 allow for both cirle and donut sliders.

--- src/main/resources/WEB-INF/js/controller/optionsController.js
+++ src/main/resources/WEB-INF/js/controller/optionsController.js
@@ -6,10 +6,13 @@ angular.module('optLocApp').controller("optionsController", function($scope, opt
     };
 
     $scope.donutRangeConfig = {
-        range: true
+        range: true,
+        disabled: false
     };
 
-    $scope.circleRangeConfig = { };
+    $scope.circleRangeConfig = {
+        disabled: true
+    };
 
     $scope.options = {
         rangeType: $scope.rangeTypes.DONUT,
@@ -24,29 +27,37 @@ angular.module('optLocApp').controller("optionsController", function($scope, opt
         [50000, undefined]
     ];
 
-    $scope.$watch("options.population", function() {
+    var updateDonut = function() {
+        optLocService.options['range-min'] = $scope.options.donutRange[0];
+        optLocService.options['range-max'] = $scope.options.donutRange[1];
+        optLocService.refresh();
+    }
+
+    var updateCircle = function() {
+        optLocService.options['range-min'] = 0;
+        optLocService.options['range-max'] = $scope.options.circleRange;
+        optLocService.refresh();
+    }
+
+    var updatePopulation = function() {
         optLocService.options['population-min'] = populationRange[$scope.options.population][0];
         optLocService.options['population-max'] = populationRange[$scope.options.population][1];
         optLocService.refresh();
-    });
+    }
 
-    $scope.$watch("options.donutRange", function() {
-        optLocService.options['range-min'] = $scope.options.donutRange[0];
-        optLocService.options['range-max'] = $scope.options.donutRange[1];
-        $scope.options.circleRange = $scope.options.donutRange[1];
-        optLocService.refresh();
-    });
-
-    $scope.$watch("options.circleRange", function() {
-        optLocService.options['range-max'] = $scope.options.circleRange;
-        $scope.options.donutRange[1] = $scope.options.circleRange;
-        optLocService.refresh();
-    });
+    $scope.$watch("options.donutRange", updateDonut);
+    $scope.$watch("options.circleRange", updateCircle);
+    $scope.$watch("options.population", updatePopulation);
 
     $scope.$watch("options.rangeType", function() {
         if ($scope.options.rangeType == $scope.rangeTypes.CIRCLE) {
-            optLocService.options['range-min'] = 0;
-            $scope.options.donutRange[0] = 0;
+            $scope.circleRangeConfig.disabled = false;
+            $scope.donutRangeConfig.disabled = true;
+            updateCircle();
+        } else if ($scope.options.rangeType == $scope.rangeTypes.DONUT) {
+            $scope.donutRangeConfig.disabled = false;
+            $scope.circleRangeConfig.disabled = true;
+            updateDonut();
         }
         console.log("range type changed to " + $scope.options.rangeType);
     });
